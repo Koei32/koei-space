534b29f164622da13faf0ef1d8d4e0617b91e79a
Koei <205915319+Koei32@users.noreply.github.com>
2026-02-22 17:53:53 +0530

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# koei.space
+source code for https://koei.space
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import favicon from "$lib/assets/faviconk.svg";
+</script>
+
+<svelte:head>
+    <title>is for koei</title>
+    <link rel="icon" href={favicon} />
+</svelte:head>
+
+<h1>koei.space</h1>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;600;700&display=swap");
+    h1 {
+        font-family: "IBM Plex Mono";
+        font-weight: 400;
+        color: white;
+    }
+</style>
