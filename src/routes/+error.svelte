70509da19292d353746b5b7d5437fca16a7bb885
Koei <205915319+Koei32@users.noreply.github.com>
2026-02-22 18:22:41 +0530

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { page } from "$app/state";
+</script>
+
+<div class="error">
+    <h2>{page.status}</h2>
+    <h3>{page.error?.message}</h3>
+</div>
+
+<style>
+    * {
+        font-family: "IBM Plex Mono";
+        color: white;
+        font-weight: 300;
+        margin: 0;
+        padding: 0;
+    }
+
+    div {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
