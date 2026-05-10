803e6a21fa3444226ff8c24d9c0cc8cf36d4a0a5
Koei <205915319+Koei32@users.noreply.github.com>
2026-05-10 18:25:40 +0530

    fix: discord "copied!" text

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     let discordCopied = $state(false);
-    let showCopyBubble = $state(false);
+    // let showCopyBubble = $state(false);
 
     function copyDiscord() {
         navigator.clipboard.writeText("koei32");
@@ -15,28 +15,16 @@
 
     <div class="socials">
         <div class="discord-wrapper button8831">
-            {#if showCopyBubble}
-                <span class="copied"
+            <button class="copied" onclick={copyDiscord}>
+                <span class="copiedtext"
                     >{discordCopied ? "copied!" : "click to copy"}</span
                 >
-            {/if}
-            <a
-                onclick={copyDiscord}
-                onmouseover={() => (showCopyBubble = true)}
-                onmouseleave={() => {
-                    showCopyBubble = false;
-                    discordCopied = false;
-                }}
-                onfocus={() => (showCopyBubble = true)}
-                aria-label="discord"
-                href="#"
-            >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                         d="M18.59 5.88997C17.36 5.31997 16.05 4.89997 14.67 4.65997C14.5 4.95997 14.3 5.36997 14.17 5.69997C12.71 5.47997 11.26 5.47997 9.83001 5.69997C9.69001 5.36997 9.49001 4.95997 9.32001 4.65997C7.94001 4.89997 6.63001 5.31997 5.40001 5.88997C2.92001 9.62997 2.25001 13.28 2.58001 16.87C4.23001 18.1 5.82001 18.84 7.39001 19.33C7.78001 18.8 8.12001 18.23 8.42001 17.64C7.85001 17.43 7.31001 17.16 6.80001 16.85C6.94001 16.75 7.07001 16.64 7.20001 16.54C10.33 18 13.72 18 16.81 16.54C16.94 16.65 17.07 16.75 17.21 16.85C16.7 17.16 16.15 17.42 15.59 17.64C15.89 18.23 16.23 18.8 16.62 19.33C18.19 18.84 19.79 18.1 21.43 16.87C21.82 12.7 20.76 9.08997 18.61 5.88997H18.59ZM8.84001 14.67C7.90001 14.67 7.13001 13.8 7.13001 12.73C7.13001 11.66 7.88001 10.79 8.84001 10.79C9.80001 10.79 10.56 11.66 10.55 12.73C10.55 13.79 9.80001 14.67 8.84001 14.67ZM15.15 14.67C14.21 14.67 13.44 13.8 13.44 12.73C13.44 11.66 14.19 10.79 15.15 10.79C16.11 10.79 16.87 11.66 16.86 12.73C16.86 13.79 16.11 14.67 15.15 14.67Z"
                     />
                 </svg>
-            </a>
+            </button>
         </div>
         <a href="https://github.com" target="_blank" aria-label="github">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 25 25"
@@ -77,18 +65,36 @@
         color: var(--fg-color);
         font-family: var(--pri-font);
         font-weight: 100;
-        /* -webkit-text-stroke: 1px var(--bg-color-sec); */
     }
 
     .copied {
-        padding: 5px;
-        border-radius: 2px;
+        position: relative;
+        background-color: #000000;
+        border: none;
+        display: inline-block;
+        cursor: pointer;
+    }
+
+    .copiedtext {
+        visibility: hidden;
+        opacity: 0;
+        width: 110px;
+        background-color: rgba(255, 255, 255, 0.6);
         font-family: "IBM Plex Mono";
         font-size: 0.75rem;
-        background-color: rgba(255, 255, 255, 0.6);
+        text-align: center;
+        padding: 5px 0;
+        border-radius: 4px;
         position: absolute;
-        top: 3rem;
-        right: 3rem;
+        z-index: 1;
+        top: 100%;
+        left: -175%;
+        transition: opacity 0.2s;
+    }
+
+    .copied:hover .copiedtext {
+        visibility: visible;
+        opacity: 100%;
     }
 
     .koei {
